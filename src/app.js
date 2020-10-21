48f82ea3d81929d566d0cf3ae02afc98e8c9b284
valdljr <valdir.lago@bling.com.br>
2020-10-21 20:59:03 -0300

    feat: repositories crud

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,52 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(newRepo);
+  return response.status(201).json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repoIndex = repositories.findIndex(repo => repo.id == request.params.id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  const updatedRepo = repositories[repoIndex];
+  updatedRepo.title = title;
+  updatedRepo.url = url;
+  updatedRepo.techs = techs;
+  repositories[repoIndex] = updatedRepo;
+  return response.status(200).json(updatedRepo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id == request.params.id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  repositories.splice(repoIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id == request.params.id);
+  if (repoIndex < 0) {
+    return response.status(400).send();
+  }
+  repositories[repoIndex].likes += 1;
+  return response.status(200).json(repositories[repoIndex]);
 });
 
 module.exports = app;
